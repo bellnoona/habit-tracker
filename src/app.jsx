2f9ed0d61562eefd0f69bfb4d534d0746ef6069d
Soohyeon Kim <tngusglaso@gmail.com>
2022-06-22 00:42:03 +0900

    JSX 정리

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,16 @@ import React from 'react'; // 유무 무슨차이? => 원래 없었음
 import './app.css';
 
 function App() {
-  return <h1>Hello:)</h1>;
+  const name = 'ellie';
+  return (
+    <>
+      <h1>Hello</h1>
+      {name && <h1>Hello! {name}:)</h1>}
+      {['❤️', '🍎'].map((item) => (
+        <h1>{item}</h1>
+      ))}
+    </>
+  );
 }
 
 export default App;
