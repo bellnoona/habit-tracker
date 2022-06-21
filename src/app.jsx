25d782f37a8328ba7eee89ec21a04616c16d6d3c
Soohyeon Kim <tngusglaso@gmail.com>
2022-06-22 01:40:21 +0900

    state 이해하기

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,9 @@
 import React from 'react'; // 유무 무슨차이? => 원래 없었음
 import './app.css';
+import Habit from './components/habit';
 
 function App() {
-  const name = 'ellie';
-  return (
-    <>
-      <h1>Hello</h1>
-      {name && <h1>Hello! {name}:)</h1>}
-      {['❤️', '🍎'].map((item) => (
-        <h1>{item}</h1>
-      ))}
-    </>
-  );
+  return <Habit />;
 }
 
 export default App;
